f5fcf04631c391c9a964a229597c163984c810c4
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-17 16:26:20 +0530

    first

--- Merge Sorted Array.js
+++ Merge Sorted Array.js
@@ -9,7 +9,9 @@ var merge = function(nums1, m, nums2, n) {
     while (i >= 0 && j >= 0) {
         if (nums1[i] > nums2[j]) {
             nums1[k--] = nums1[i--];
-        
+        } else {
+            nums1[k--] = nums2[j--];
+        }
     }
     while (j >= 0) {
         nums1[k--] = nums2[j--];
