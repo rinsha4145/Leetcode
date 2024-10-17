5a6b44767cdaa1a7241f000a35c9ce53d483591b
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-17 16:24:32 +0530

    first

--- Remove Element.js
+++ Remove Element.js
@@ -14,7 +14,7 @@ var removeElement = function(nums, val) {
 // Call the function and pass `nums` and `val`
 removeElement(nums, val);
 
-
+// Print `k` which represents the new length of the array after removal
 console.log(k);  // Output: 2
 
-
+  // Output: [2, 2]
