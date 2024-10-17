4def3227cc391ae75568292da6cff3715cd4275b
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-17 16:25:55 +0530

    first

--- Merge Sorted Array.js
+++ Merge Sorted Array.js
@@ -1,7 +1,7 @@
 let nums1 = [1,2,3,0,0,0]
- let m = 3
- let  nums2 = [2,5,6]
-  let n = 3
+let m = 3
+let  nums2 = [2,5,6]
+let n = 3
 var merge = function(nums1, m, nums2, n) {
     let i = m - 1;
     let j = n - 1;
@@ -16,6 +16,5 @@ var merge = function(nums1, m, nums2, n) {
     while (j >= 0) {
         nums1[k--] = nums2[j--];
     }
-    
 };
 console.log(nums1)
