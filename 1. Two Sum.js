d35a9fa743618b0d92733960d95b0dee354e2bc2
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-19 09:52:55 +0530

    first

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -4,9 +4,10 @@ var twoSum = function(nums, target) {
     for(i=0;i<nums.length;i++){
         for(j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]===target){
-                return [i,j]
+                console.log([i,j])
             }
         }
     }
     
 };
+twoSum(nums,target)
