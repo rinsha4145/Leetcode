63e0f5aa9d57dc7caf2f1561ad808c561d4e26b5
rinsha4145 <rinshajouhar5181@gmail.com>
2024-10-31 14:09:22 +0530

    first

--- 2648. Generate Fibonacci Sequence.js
+++ 2648. Generate Fibonacci Sequence.js
@@ -1,8 +1,12 @@
-let fn = (x) => x * 5
-let args = [2]
-let t = 20
+let fn = (x) => x * 5;
+let args = [2];
+let t = 20;
+
 var cancellable = function(fn, args, t) {
-    const t1 = setTimeout(()=>fn(...args), t);
-    return () => clearTimeout(t1)
+    const t1 = setTimeout(() => {
+        console.log(fn(...args)); // Log the result of fn
+    }, t);
+    return () => clearTimeout(t1);
 };
-console.log(cancellable())
+
+const cancel = cancellable(fn, args, t); 
