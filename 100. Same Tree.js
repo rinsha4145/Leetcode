507083f6e09c8b7283d406836ca11ce5c64294e6
rinsha4145 <rinshajouhar5181@gmail.com>
2024-11-23 15:33:55 +0530

    Remove 100.same tree.js from the repository

--- 100. Same Tree.js
+++
@@ -1,7 +0,0 @@
-
-let p = [1,2,3], q = [1,2,3]
-var isSameTree = function(p, q) {
-    if(!p && !q) return true 
-    if(!p || !q|| p.val!==q.val) return false
-    return (isSameTree(p.left,q.left)) &&  (isSameTree(p.right,q.right))
-};
