fee8b81998392d54b188ea32e70a37ca1ca86c71
rinsha4145 <rinshajouhar5181@gmail.com>
2024-11-26 22:21:41 +0530

    first

---
+++ 100. Same Tree.js
@@ -0,0 +1,7 @@
+
+let p = [1,2,3], q = [1,2,3]
+var isSameTree = function(p, q) {
+    if(!p && !q) return true 
+    if(!p || !q|| p.val!==q.val) return false
+    return (isSameTree(p.left,q.left)) &&  (isSameTree(p.right,q.right))
+};
