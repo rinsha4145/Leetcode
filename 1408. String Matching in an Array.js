d25741b283dfb144e156e587ae9941150f163837
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-16 19:24:20 +0530

    1408

---
+++ 1408. String Matching in an Array.js
@@ -0,0 +1,12 @@
+var stringMatching = function(words) {
+    let z=[]
+   for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length; j++) {
+            if (i !== j && words[j].includes(words[i])) {
+                z.push(words[i]);
+                break; 
+            }
+        }
+    }
+    return z
+};
