1007a95f8f2b1b1b04c719486fa29101496effde
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-18 09:19:52 +0530

    7

--- 7. Reverse Integer.js
+++ 7. Reverse Integer.js
@@ -1,10 +1,9 @@
-var reverse = function(x) {
-    const reversedString = Math.abs(x).toString().split('').reverse().join('');
-    const reversedNumber = parseInt(reversedString);
-    if (reversedNumber > Math.pow(2, 31) - 1) {
-        return 0; 
-    }
-    return x < 0 ? -reversedNumber : reversedNumber;
-    
+var reverse = function (x) {
+  const reversedString = Math.abs(x).toString().split("").reverse().join("");
+  const reversedNumber = parseInt(reversedString);
+  if (reversedNumber > Math.pow(2, 31) - 1) {
+    return 0;
+  }
+  return x < 0 ? -reversedNumber : reversedNumber;
 };
-console.log(reverse(123))
+console.log(reverse(123));
