1669f76883c414cc8bb4b9b7973df6de9accf556
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-18 09:21:19 +0530

    9

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(x) {
+    const y=x.toString().split('').reverse().join('');
+    if(x==y){
+        return true
+    }
+    else 
+        return false
+    
+};
+console.log(isPalindrome(121))
