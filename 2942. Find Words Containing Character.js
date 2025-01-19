ac2d1696999735243b40bf30eebee642987c1dad
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-19 18:28:15 +0530

    2942

---
+++ 2942. Find Words Containing Character.js
@@ -0,0 +1,9 @@
+var findWordsContaining = function(words, x) {
+    let a=[]
+    for(let i=0; i<words.length;i++)
+        if(words[i].includes(x))
+            a.push(i)
+    
+    return a
+};
+console.log(findWordsContaining(["leet","code"],"e"))
