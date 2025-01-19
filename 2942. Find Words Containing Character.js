d9f9271c8fd4395d40ded9924a91bf05f3c75355
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-19 18:28:25 +0530

    2942

--- 2942. Find Words Containing Character.js
+++ 2942. Find Words Containing Character.js
@@ -1,9 +1,7 @@
-var findWordsContaining = function(words, x) {
-    let a=[]
-    for(let i=0; i<words.length;i++)
-        if(words[i].includes(x))
-            a.push(i)
-    
-    return a
+var findWordsContaining = function (words, x) {
+  let a = [];
+  for (let i = 0; i < words.length; i++) if (words[i].includes(x)) a.push(i);
+
+  return a;
 };
-console.log(findWordsContaining(["leet","code"],"e"))
+console.log(findWordsContaining(["leet", "code"], "e"));
