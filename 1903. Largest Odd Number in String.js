359854adaa37a4bddf14eeb42c96ce4ae3721a18
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-20 09:17:33 +0530

    1903

--- 1903. Largest Odd Number in String.js
+++ 1903. Largest Odd Number in String.js
@@ -1,8 +1,8 @@
-var largestOddNumber = function(num) {
-    if(Number(num) %2===1) return num;
-    const item = num.split('').map((item) => Number(item))
-    for(let i=item.length-1; i >= 0 ; i--)
-        if(item[i] %2 === 1) return num.slice(0,i+1);
-    return "";
+var largestOddNumber = function (num) {
+  if (Number(num) % 2 === 1) return num;
+  const item = num.split("").map((item) => Number(item));
+  for (let i = item.length - 1; i >= 0; i--)
+    if (item[i] % 2 === 1) return num.slice(0, i + 1);
+  return "";
 };
-console.log(largestOddNumber("52"))
+console.log(largestOddNumber("52"));
