2f1517e3e00cb7bb55b3d6aabc0dae9207cf3910
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-21 10:07:14 +0530

    349

--- 349. Intersection of Two Arrays.js
+++ 349. Intersection of Two Arrays.js
@@ -1,10 +1,7 @@
-var intersection = function(nums1, nums2) {
-    let a=[]
-    for(let i of nums1)
-        if(nums2.includes(i))
-            a.push(i)
-    let set=new Set(a)
-    return [...set]
-
+var intersection = function (nums1, nums2) {
+  let a = [];
+  for (let i of nums1) if (nums2.includes(i)) a.push(i);
+  let set = new Set(a);
+  return [...set];
 };
-console.log(intersection([1,2,2,1],[2,2]))
+console.log(intersection([1, 2, 2, 1], [2, 2]));
