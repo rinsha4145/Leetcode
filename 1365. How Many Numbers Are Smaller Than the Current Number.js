c25daafac8c37c4d175aa06758f99e0c08318212
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-22 09:00:58 +0530

    1365

--- 1365. How Many Numbers Are Smaller Than the Current Number.js
+++ 1365. How Many Numbers Are Smaller Than the Current Number.js
@@ -1,5 +1,5 @@
-var smallerNumbersThanCurrent = function(nums) {
-    const sorted = [...nums].sort((a, b) => a - b);
-    return nums.map(num => sorted.indexOf(num));
+var smallerNumbersThanCurrent = function (nums) {
+  const sorted = [...nums].sort((a, b) => a - b);
+  return nums.map((num) => sorted.indexOf(num));
 };
-console.log(smallerNumbersThanCurrent([8,1,2,2,3]))
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
