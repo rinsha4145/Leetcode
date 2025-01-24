8e8bd7788963aac7eb825a36f508f27e4008a209
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-24 14:18:26 +0530

    2427

---
+++ 2427. Number of Common Factors.js
@@ -0,0 +1,8 @@
+var commonFactors = function(a, b) {
+    let count = 0;
+   for (let i = 1; i <= Math.min(a, b); i++) {
+     a % i=== 0 && b % i=== 0?count++:0
+   }
+   return count;
+ };
+ console.log(first)
