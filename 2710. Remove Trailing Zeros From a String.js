75d6834d6fff2d312cc693e94fc2c3034a21ad30
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-25 10:06:01 +0530

    2710

--- 2710. Remove Trailing Zeros From a String.js
+++ 2710. Remove Trailing Zeros From a String.js
@@ -1,9 +1,8 @@
-var removeTrailingZeros = function(num) {
-   
-    let arr = num.split("")
-for (let i = 0; i <num.length-1; i++) {
-    if (arr[arr.length - 1] == 0) arr.pop()
-}
-return arr.join("")
+var removeTrailingZeros = function (num) {
+  let arr = num.split("");
+  for (let i = 0; i < num.length - 1; i++) {
+    if (arr[arr.length - 1] == 0) arr.pop();
+  }
+  return arr.join("");
 };
-console.log(removeTrailingZeros("5287100"))
+console.log(removeTrailingZeros("5287100"));
