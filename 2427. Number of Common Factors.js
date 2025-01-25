ce8ac8364733538d014131a0bca0aaab1e437a78
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-25 10:04:52 +0530

    2710

--- 2427. Number of Common Factors.js
+++ 2427. Number of Common Factors.js
@@ -1,8 +1,8 @@
-var commonFactors = function(a, b) {
-    let count = 0;
-   for (let i = 1; i <= Math.min(a, b); i++) {
-     a % i=== 0 && b % i=== 0?count++:0
-   }
-   return count;
- };
- console.log(commonFactors(12,6))
+var commonFactors = function (a, b) {
+  let count = 0;
+  for (let i = 1; i <= Math.min(a, b); i++) {
+    a % i === 0 && b % i === 0 ? count++ : 0;
+  }
+  return count;
+};
+console.log(commonFactors(12, 6));
---
+++ 2710. Remove Trailing Zeros From a String.js
@@ -0,0 +1,8 @@
+var removeTrailingZeros = function(num) {
+   
+    let arr = num.split("")
+for (let i = 0; i <num.length-1; i++) {
+    if (arr[arr.length - 1] == 0) arr.pop()
+}
+return arr.join("")
+};
