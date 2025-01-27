d78c7c04f054e4c8aea7d05deb3af06a673e44b1
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-27 08:51:42 +0530

    1909

---
+++ 1909. Remove One Element to Make the Array Strictly Increasing.js
@@ -0,0 +1,16 @@
+var canBeIncreasing = function(nums) {
+    let removed = false;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] >= nums[i + 1]) {
+            if (removed) return false; 
+
+            if (i > 0 && nums[i - 1] >= nums[i + 1] && i + 2 < nums.length && nums[i] >= nums[i + 2]) {
+                return false;
+            }
+
+            removed = true; 
+        }
+    }
+
+    return true;
+};
