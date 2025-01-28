dbabfe51ae56f9f4474598bba1a04b2f25f676f6
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-28 08:40:53 +0530

    2544

---
+++ 2544. Alternating Digit Sum.js
@@ -0,0 +1,18 @@
+
+var alternateDigitSum = function(n) {
+    let isPositive = true;
+    let digits = n.toString().split(''); 
+    let sum = 0;
+    for (let digit of digits) {
+        let num = parseInt(digit); 
+        if (isPositive) {
+            sum += num;
+            isPositive = false;
+        } else {
+            sum -= num;
+            isPositive = true;
+        }
+    }
+
+    return sum;
+};
