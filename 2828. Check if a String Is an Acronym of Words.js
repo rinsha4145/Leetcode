0a9132c05beff2b0f804d9c6fa0a95d7a5d2589d
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-29 08:53:57 +0530

    2828

--- 2828. Check if a String Is an Acronym of Words.js
+++ 2828. Check if a String Is an Acronym of Words.js
@@ -1,10 +1,10 @@
-var isAcronym = function(words, s) {
-    if (words.length !== s.length) return false;
-    for (let i = 0; i < words.length; i++) {
-       if (!words[i].startsWith(s[i])) {
-           return false;
-       }
-   }
-   return true
+var isAcronym = function (words, s) {
+  if (words.length !== s.length) return false;
+  for (let i = 0; i < words.length; i++) {
+    if (!words[i].startsWith(s[i])) {
+      return false;
+    }
+  }
+  return true;
 };
-console.log(isAcronym(["alice","bob","charlie"],"abc"))
+console.log(isAcronym(["alice", "bob", "charlie"], "abc"));
