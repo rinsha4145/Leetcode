05a90b28a6830549322d0739d489155670bbc15b
rinsha4145 <rinshajouhar5181@gmail.com>
2025-01-29 08:53:13 +0530

    2828

---
+++ 2828. Check if a String Is an Acronym of Words.js
@@ -0,0 +1,9 @@
+var isAcronym = function(words, s) {
+    if (words.length !== s.length) return false;
+    for (let i = 0; i < words.length; i++) {
+       if (!words[i].startsWith(s[i])) {
+           return false;
+       }
+   }
+   return true
+};
